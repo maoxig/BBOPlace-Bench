13d447052eee410dd57fe89ed400cd53e63fbbcf
maoxig <674550338@qq.com>
2026-01-31 23:51:23 +0800

    fix: 使用bin来获取rudy2避免OOM

--- moscripts/MO_OPENROAD_MGO_SMSEMOA_MP.sh
+++ moscripts/MO_OPENROAD_MGO_SMSEMOA_MP.sh
@@ -18,7 +18,7 @@ python ../src/main.py \
     --sampling=random \
     --mutation=shuffle \
     --crossover=uniform \
-    --eval_metrics='["hpwl","regularity","rudy"]' \
+    --eval_metrics='["hpwl","regularity","rudy2"]' \
     --eval_gp_hpwl=False \
     --n_max_saving_placement=5 \
     --verbose=False
